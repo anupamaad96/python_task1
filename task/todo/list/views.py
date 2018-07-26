c88acba52fc4ca2851b840d2697bad0e5a1e54e3
anupamaad96 <41427386+anupamaad96@users.noreply.github.com>
2018-07-26 11:53:57 +0530

    Update views.py

--- task/todo/list/views.py
+++ task/todo/list/views.py
@@ -4,12 +4,46 @@ from django.shortcuts import render
 from models import TaskModel
 from datetime import datetime
 
-def create(request):
+@csrf_exempt
+def create_task(request):
     try:
         if request.method=='POST':
-            title = request.data('title')
-            description = request.data.get('description')
-            completion_date = datetime.strptime(request.data[completion_date],%d/%m/%y %H:%S )
+            title = request.POST('title')
+            description = request.POST.get('description')
+            completion_date = datetime.strptime(request.POST[completion_date],%d/%m/%Y %H:%M )
+            priority = request.POST['priority']
             TaskModel.objects.create(title=title,description=description,completion_date=completion_date,priority=priority)
-    except Exception:
-        pass
+    except Exception as e:
+        raise e
+@csrf_exempt
+def edit_task(request,pk=None):
+    try:
+        if request.method=='POST':
+            task = TaskModel.objects.filter(id=pk).first()
+            title = request.POST('title')
+            description = request.POST.get('description')
+            completion_date = datetime.strptime(request.POST[completion_date],%d%m%Y %H:%M)
+            task.title = title
+            task.description = description
+            task.completion_date = completion_date
+            task.priority = priority
+            task.save()
+    except Exception as e:
+        raise e
+        
+@csrf_exempt
+def view_incomplete_task(request):
+    try:
+        if request.method == 'GET':
+            incomplete_tasks = TaskModel.objects.filter(is_completed=False)
+            incomplete_tasks_list = [dict(title=task.title,
+                                          completion_date=task.completion_date.strftime('%d/%m/%Y %H:%M'))
+                                     for task in incomplete_tasks]
+    except Exception as e:
+        raise e    
+@csrf_exempt
+def delete(request, id):
+   delpost = TaskModel.objects.get(pk = id)
+   delpost.delete()
+   
+
